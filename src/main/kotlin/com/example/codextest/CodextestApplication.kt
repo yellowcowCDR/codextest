bb3490623e4288667599ddef8238ed08b5a861bb
yellowcowCDR <eofus4250@naver.com>
2025-06-14 23:14:45 +0900

    initial commit

---
+++ src/main/kotlin/com/example/codextest/CodextestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.codextest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodextestApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodextestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/codextest/CodextestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.codextest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodextestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
